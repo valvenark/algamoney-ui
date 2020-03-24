b800f836936c8af034c7c061ca5fb297535d7a6d
valvenark <valvenark@gmail.com>
2020-03-24 20:49:26 -0300

    Novos Formulários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,44 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { InputMaskModule } from 'primeng/inputmask';
+import { TooltipModule } from 'primeng/tooltip';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CalendarModule } from 'primeng/calendar';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
 
 import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PesquisaPessoaComponent } from './pesquisa-pessoa/pesquisa-pessoa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent,
+    PesquisaPessoaComponent,
+    LancamentoCadastroComponent
   ],
   imports: [
     BrowserModule,
-    InputTextModule,
     ButtonModule,
+    BrowserAnimationsModule,
+    CalendarModule,
+    DropdownModule,
+    InputMaskModule,
+    InputTextModule,
+    InputTextareaModule,
+    SelectButtonModule,
     TableModule,
+    TooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent {
+ 
+  pt_br: any;
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' },
+  ]
+
+  categorias = [
+    { label: 'Alimentação', value: 1 },
+    { label: 'Trasporte', value: 2 },
+  ]
+
+  pessoas = [
+    { label: 'João da Silva', value: 1 },
+    { label: 'Sebastião Souza', value: 2 },
+    { label: 'Maria Abadia', value: 3 },
+  ]
+
+  constructor() { }
+
+    ngOnInit() {
+        this.pt_br = {
+            firstDayOfWeek: 0,
+            dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+            dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+            dayNamesMin: ["D","S","T","Q","Q","S","S"],
+            monthNames: [ "Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro" ],
+            monthNamesShort: [ "Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul", "Ago", "Set", "Out", "Nov", "Dez" ],
+            today: 'Hoje',
+            clear: 'Limpar',
+            dateFormat: 'mm/dd/yy',
+            weekHeader: 'Semana'
+        };
+    }
+
+}
